b5dad865d95b8ee6f0ccc1e719c2f0949a621b5b
mattiabenelli <benellimattia0@gmail.com>
2023-05-24 16:41:58 +0200

    step one corrected

--- src/main/java/org/java/best/controller/MyController.java
+++ src/main/java/org/java/best/controller/MyController.java
@@ -5,17 +5,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 @RequestMapping("/")
 public class MyController {
 	
-	@GetMapping("/ciao/{nome}")
+//	private List<String> movie = new ArrayList<>();
+	
+	
+	@GetMapping("/ciao")
 	public String sayHello(Model model,
-							@PathVariable("nome") String nome) {
+							@RequestParam(name = "name") String name) {
 		
-		model.addAttribute(nome);
+		model.addAttribute("name", name);
 		
 		return "index";
 	}
